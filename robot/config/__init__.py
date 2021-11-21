589e6062df8b8b94ca6161770bb2b3ea17ff62aa
Yan Toledo <yan.toledo@hotmail.com>
2021-11-21 13:52:01 -0300

    add config file

---
+++ robot/config/__init__.py
@@ -0,0 +1,1 @@
+from robot.config import config
---
+++ robot/config/config.py
@@ -0,0 +1,5 @@
+LOGIN = ""
+PASSWORD = ""
+ACCOUNT_TYPE = "PRACTICE"  # Can be PRACTICE or REAL
+PAIR = "EURUSD"  # Defines the pair of coins that bot will operates
+TIMEFRAME = 1
